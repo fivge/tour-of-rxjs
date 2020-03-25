198172fbcfd70bd91d5081eaaa29102acd8c95c9
fivge <luanxingtong@gmail.com>
2020-03-25 15:43:22 +0800

    feat: add rxjs support
    tip: use rxjs/operators failed

--- require-config.js
+++ require-config.js
@@ -1,6 +1,7 @@
 requirejs.config({
   paths: {
     react: ["/node_modules/react/umd/react.development"],
-    "react-dom": ["/node_modules/react-dom/umd/react-dom.development"]
+    "react-dom": ["/node_modules/react-dom/umd/react-dom.development"],
+    rxjs: ["/node_modules/rxjs/bundles/rxjs.umd"]
   }
 });
--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Greeter as Greetifier, GreeterProps } from "./brower/greeter";
 
+import { of } from "rxjs";
+// import { map } from "rxjs/operators";
+
 function getRandomGreeting() {
+  let source = of({ name: "superman" });
+  // let opt = source.pipe(map(v => (v.name = "speedman")));
+  source.subscribe(console.log);
   switch (Math.floor(Math.random() * 4)) {
     case 0:
       return "Hello";
