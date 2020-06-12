2d5629b63328713dc823e21334bc21b0f1e82421
栾兴通 <luanxingtong@inspur.com>
2020-06-12 16:28:45 +0800

    feat: update from, of, map operators

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 #### dev
 
 ```bash
+### nodejs
+#### dev
 yarn build:dev
-
-yarn start
-```
-
 #### prod
-
-```bash
 yarn build:prod
+yarn start
+### web
+yarn build:web
+cd public && serve
 ```
 
 主函数调用使用 rxjs 中的 main 函数
+
+#### tree
+
+##### 1
+
+- of from
+- operators: map()
+- javascript: array.map()
--- src/module/rxjs.1.ts
+++ src/module/rxjs.1.ts
@@ -1,31 +1,65 @@
 /**
- * from of
+ * of from
  * operators: map()
  * javascript: array.map()
  */
-import { from, of } from "rxjs";
-import { map } from "rxjs/operators";
+import { from, of } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 
-import { Rxjs } from "./rxjs";
+import { Rxjs } from './rxjs';
 
 /** map() */
 export class Rxjs1 implements Rxjs {
   main() {
+    // this.funOf();
+    // this.funFrom();
     this.funMapFrom();
     this.funMapOf();
   }
 
+  private funOf = () => {
+    const source = of(1, 2, 3, 4, 5, { name: 'Brian' }, [1, 2, 3], function hello() {
+      return 'Hello';
+    });
+    // 输出：1, 2, 3, 4, 5, { name: 'Brian' }, [1, 2, 3], function hello() {return 'Hello';}
+    source.subscribe(console.log);
+  };
+
+  private funFrom = () => {
+    const source = from([1, 2, 3, 4, 5, { name: 'Brian' }, 'foooooo']);
+    // 输出：1, 2, 3, 4, 5, { name: 'Brian' }, 'foooooo'
+    source.subscribe(console.log);
+
+    // 字符串
+    // 输出：f, o, o, o, o, o
+    from('fooooo').subscribe(console.log);
+
+    // 迭代器
+    function* generateDoubles(seed) {
+      var i = seed;
+      while (true) {
+        yield i;
+        i = 2 * i; // double it
+      }
+    }
+    // 输出：3, 6, 12, 24, 48, 96, 192, 384, 768, 1536
+    from(generateDoubles(3)).pipe(take(10)).subscribe(console.log);
+  };
+
   private funMapFrom = () => {
     const source = from([1, 2, 3, 4, 5]);
     const example = source.pipe(map(val => val + 10));
+    // 输出: 1, 2, 3, 4, 5
+    // 不会改变已经存在的 Observable 实例
+    source.subscribe(res => console.log('from', res));
     // 输出: 11,12,13,14,15
-    const subscribe = example.subscribe(console.log);
+    example.subscribe(res => console.log('from map', res));
   };
 
   private funMapOf = () => {
-    const source = of([1, 2, 3, 4, 5]);
+    const source = of([1, 2, 3, 4, 5], [6, 7, 8, 9]);
     const example = source.pipe(map(vals => vals.map(val => val + 10)));
-    // 输出: [ 11, 12, 13, 14, 15 ]
-    const subscribe = example.subscribe(console.log);
+    // 输出: [ 11, 12, 13, 14, 15 ], [ 16, 17, 18, 19 ]
+    example.subscribe(console.log);
   };
 }
